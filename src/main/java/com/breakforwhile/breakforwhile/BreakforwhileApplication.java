c9ab1217764621c5ce58e847026087b9a5e7886e
Rafael Gomes <faelfg@gmail.com>
2021-02-20 00:31:00 -0300

    Initial commit

---
+++ src/main/java/com/breakforwhile/breakforwhile/BreakforwhileApplication.java
@@ -0,0 +1,13 @@
+package com.breakforwhile.breakforwhile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakforwhileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakforwhileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breakforwhile/breakforwhile/BreakforwhileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakforwhile.breakforwhile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakforwhileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
